352e6ae9bcca68ecb4a2a861c8c575b21282c3a1
Poorvaa Wasnik <poorvaawasnik@gmail.com>
2025-11-24 12:36:29 +0530

    Add New Configs

---
+++ src/main/java/com/codingshuttle/anuj/module1introduction/AppConfig2.java
@@ -0,0 +1,9 @@
+package com.codingshuttle.anuj.module1introduction;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig2 {
+
+}
